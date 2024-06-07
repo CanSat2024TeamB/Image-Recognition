de76be329db02d0a9e5144622f383f5c1ff0dac3
nekochok0 <kkotaro2872@gmail.com>
2024-06-08 06:11:11 +0900

    add clock

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 #include "cascade_handler.h"
 
 int main() {
 	std::string cascade_path = "haarcascade_frontalface_alt.xml";
-	std::string img_path = "face_sample.jpg";
+	std::string img_path = "face_sample2.jpg";
 
 	cascade_handler face_cascade(cascade_path);
 	cv::Mat img = cv::imread(img_path);
 
+	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+
 	cv::Mat face = face_cascade.draw_rect(img);
+
+	std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+
+	std::chrono::duration<double, std::milli> elapsed = end - start;
+
+	std::cout << elapsed.count() << " ms" << std::endl;
 
 	cv::imshow("face", face);
 	cv::waitKey();
