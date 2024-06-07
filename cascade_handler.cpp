71a806bbc307d1c71dacb3ef9870aeaafe2754c4
nekochok0 <kkotaro2872@gmail.com>
2024-06-08 04:13:51 +0900

    add rect func

--- cascade_handler.cpp
+++ cascade_handler.cpp
@@ -2,22 +2,37 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 
-std::vector<cv::Rect> cascade_handler::apply(cv::Mat raw_img, double scale_factor = 1.1, int min_nerghbors = 3, cv::Size min_size = cv::Size()) {
-	cv::Mat gray_img;
-	cv::cvtColor(raw_img, gray_img, cv::COLOR_BGR2GRAY);
+cascade_handler::cascade_handler(std::string path) {
+	set_cascade(path);
+}
 
-	std::vector<cv::Rect> custom_rect;
-	cascade.detectMultiScale(gray_img, custom_rect, scale_factor, min_nerghbors, 0, min_size);
+void cascade_handler::set_cascade(std::string path) {
+	cascade.load(path);
 
-	return custom_rect;
+	return;
 }
 
 cv::CascadeClassifier cascade_handler::get_cascade() {
 	return this->cascade;
 }
 
-void cascade_handler::set_cascade(std::string path) {
-	this->cascade.load(path);
+std::vector<cv::Rect> cascade_handler::get_rect(cv::Mat raw_img, double scale_factor = 1.1, int min_neighbors = 3, cv::Size min_size = cv::Size()) {
+	cv::Mat gray_img;
+	cv::cvtColor(raw_img, gray_img, cv::COLOR_BGR2GRAY);
 
-	return;
+	std::vector<cv::Rect> custom_rect;
+	cascade.detectMultiScale(gray_img, custom_rect, scale_factor, min_neighbors, 0, min_size);
+
+	return custom_rect;
+}
+
+cv::Mat cascade_handler::draw_rect(cv::Mat raw_img, double scale_factor = 1.1, int min_neighbors = 3, cv::Size min_size = cv::Size()) {
+	cv::Mat dst_img = raw_img.clone();
+
+	std::vector<cv::Rect> rects = get_rect(raw_img, scale_factor, min_neighbors, min_size);
+	for (cv::Rect rect : rects) {
+		cv::rectangle(dst_img, rect, cv::Scalar(255, 0, 0), 3);
+	}
+
+	return dst_img;
 }
--- cascade_handler.h
+++ cascade_handler.h
@@ -5,8 +5,14 @@
 class cascade_handler {
 private:
 	cv::CascadeClassifier cascade;
+
 public:
-	std::vector<cv::Rect> apply(cv::Mat raw_img, double scale_factor = 1.1, int min_nerghbors = 3, cv::Size min_size = cv::Size());
-	cv::CascadeClassifier get_cascade();
+	cascade_handler(std::string path);
+
 	void set_cascade(std::string path);
+
+	cv::CascadeClassifier get_cascade();
+
+	std::vector<cv::Rect> get_rect(cv::Mat raw_img, double scale_factor = 1.1, int min_neighbors = 3, cv::Size min_size = cv::Size());
+	cv::Mat draw_rect(cv::Mat raw_img, double scale_factor = 1.1, int min_neighbors = 3, cv::Size min_size = cv::Size());
 };
