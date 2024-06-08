f47e83b1fdbf11a2d8f5f04763124fd0c6ce0073
nekochoc0 <61615276+nekochoc0@users.noreply.github.com>
2024-06-09 03:25:32 +0900

    Camera (#5)
    * add camera_handler
    
    * change main to output pics as a video

--- main.cpp
+++ main.cpp
@@ -3,26 +3,50 @@
 #include <chrono>
 #include <opencv2/opencv.hpp>
 #include "cascade_handler.h"
+#include "camera_handler.h"
 
 int main() {
 	std::string cascade_path = "haarcascade_frontalface_alt.xml";
-	std::string img_path = "face_sample2.jpg";
+	//std::string img_path = "face_sample2.jpg";
 
 	cascade_handler face_cascade(cascade_path);
-	cv::Mat img = cv::imread(img_path);
+	camera_handler camera(0);
 
-	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+	//cv::Mat img = cv::imread(img_path);
+	//cv::Mat img = camera.capture();
 
-	cv::Mat face = face_cascade.draw_rect(img);
+	//std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
 
-	std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+	//cv::Mat face = face_cascade.draw_rect(img);
 
-	std::chrono::duration<double, std::milli> elapsed = end - start;
+	//std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
 
-	std::cout << elapsed.count() << " ms" << std::endl;
+	//std::chrono::duration<double, std::milli> elapsed = end - start;
 
-	cv::imshow("face", face);
-	cv::waitKey();
+	//std::cout << elapsed.count() << " ms" << std::endl;
+
+	//cv::imshow("face", face);
+	//cv::waitKey();
+
+	while (true) {
+		cv::Mat img = camera.capture();
+
+		std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+
+		cv::Mat face = face_cascade.draw_rect(img);
+
+		std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+
+		std::chrono::duration<double, std::milli> elapsed = end - start;
+
+		std::cout << elapsed.count() << " ms" << std::endl;
+		cv::imshow("video", face);
+
+		int key = cv::waitKey(1);
+		if (key == 'q') {
+			break;
+		}
+	}
 
 	return 0;
 }
